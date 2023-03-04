101537861f23d8f015f5fe48d16b974cd3c6ff58
Wattima <darwattima@gmail.com>
2023-03-04 10:06:35 +0300

    used faker to seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_054445) do
+ActiveRecord::Schema.define(version: 2023_03_04_055400) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_03_04_054445) do
     t.integer "year"
     t.text "plot"
     t.string "poster"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,25 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding movies..."
 
 # Seed your database here
+10.times do
+  # create a game with random data
+  user = User.create(
+    username: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.word
+  )
+
+  # create between 1 and 5 reviews for each game
+  rand(1..5).times do
+    Movie.create(
+      title: Faker::Movie.title,
+      director: Faker::Name.unique.name,
+      year: rand(1985..2015),
+      plot: Faker::Lorem.words,
+      poster: Faker::Quote.singular_siegler,
+      user_id: rand(1..10)
+    )
+  end
+end
 
 puts "✅ Done seeding!"
