ed8b4c3c7f5511812ca8415e5521b7899d9fe96e
Wattima <darwattima@gmail.com>
2023-03-04 14:46:49 +0300

    rename password to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_055400) do
+ActiveRecord::Schema.define(version: 2023_03_04_114312) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2023_03_04_055400) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
